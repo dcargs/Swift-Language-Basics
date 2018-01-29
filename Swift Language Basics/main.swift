d5d1f4f89cf9a68fc4f98ca624d79a92d36c7821
Devin Cargill <devin@mwc-032153.dhcp.missouri.edu>
2018-01-29 11:18:41 -0600

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Devin Cargill on 1/29/18.
+//  Copyright © 2018 Devin Cargill. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
